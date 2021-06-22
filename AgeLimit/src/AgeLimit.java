9f05adb37c5d316a54a1cf60ca53c845be2e2a8f
Joshua Luppes <cooljoebob64@gmail.com>
2021-06-22 10:51:48 -0500

    Refactor default age limit to constant and add comments

--- AgeLimit/src/AgeLimit.java
+++ AgeLimit/src/AgeLimit.java
@@ -2,25 +2,25 @@ import java.util.Scanner;
 
 public class AgeLimit {
 
-	public static void main(String[] args) {
+	final static int DEFAULT_AGE_LIMIT = 21;
 		
+	public static void main(String[] args) {		
 		
-		Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);	
+		int ageLimit;	
+		int userAge;
 		
-		int DEFAULT_AGE_LIMIT = 21;
-		
+		// Get the age limit from the user
 		System.out.println("Enter the age limit: ");
-		int ageLimit;
 		try {
 			ageLimit = Integer.parseInt(input.nextLine());
-			System.out.println("Age limit set to: " + ageLimit);
+			System.out.println("Age limit set to " + ageLimit);
 		} catch(NumberFormatException e) {
 			ageLimit = DEFAULT_AGE_LIMIT;
 			System.out.println("That's not an integer. Age limit set to " + ageLimit);
 		}
 		
-		
-		int userAge;
+		// Get the user's age from the user
 		System.out.println("Enter your age: ");
 		try {
 			userAge = Integer.parseInt(input.nextLine());
@@ -30,6 +30,7 @@ public class AgeLimit {
 			System.out.println("That's not an integer. User age set to " + userAge);
 		}
 		
+		// Check if the user is old enough, and display a message for the result
 		if(userAge>=ageLimit) {
 			System.out.println("You are of age! Enjoy!");
 		} else {
